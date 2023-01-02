11da55c5d69ac7cb97db54e9fcb349cfb98cc32d
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2023-01-02 00:18:26 -0500

    4, split porject, Data, Models, Utility, BulkyBookWeb

---
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess;
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Category> Categories { get; set; }
+}
---
+++ BulkyBook.Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace BulkyBook.Models;
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
